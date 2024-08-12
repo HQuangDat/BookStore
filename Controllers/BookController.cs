7b0d27cbf3c31335bedeb7da1f2d2c68ff86fdab
Quang Dat <hoquangdat123@gmail.com>
2024-08-12 22:10:07 +0700

    Designed view for Add book and List

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -1,54 +1,53 @@
 ﻿using BookStore.Data;
 using BookStore.Models.Entity;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace BookStore.Controllers
+public class BookController : Controller
 {
-    public class BookController : Controller
+    private readonly ApplicationDbContext dbContext;
+
+    public BookController(ApplicationDbContext dbContext)
     {
-        private readonly ApplicationDbContext dbContext;
+        this.dbContext = dbContext;
+    }
 
-        public BookController(ApplicationDbContext dbContext)
+    // GET: Book/Add
+    [HttpGet]
+    public IActionResult Add()
+    {
+        ViewBag.Authors = new SelectList(dbContext.Authors, "AuthorID", "LastName");
+        ViewBag.Categories = new SelectList(dbContext.Categories, "CategoryID", "CategoryName");
+        return View();
+    }
+
+    // POST: Book/Add
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Add(Book book)
+    {
+        if (ModelState.IsValid)
         {
-            this.dbContext = dbContext;
-        }
-
-        //Add Function
-        [HttpGet]
-        public IActionResult Add()
-        {
-            ViewBag.Authors = new SelectList(dbContext.Authors, "AuthorID", "LastName");
-            ViewBag.Categories = new SelectList(dbContext.Categories, "CategoryID", "CategoryName");
-            return View();
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Add(Book book)
-        {
-            if (!ModelState.IsValid)
-            {
-                ViewBag.Authors = new SelectList(dbContext.Authors, "AuthorID", "LastName");
-                ViewBag.Categories = new SelectList(dbContext.Categories, "CategoryID", "CategoryName");
-                return View(book);
-            }
-
             book.BookID = Guid.NewGuid();
             await dbContext.Books.AddAsync(book);
             await dbContext.SaveChangesAsync();
-            return RedirectToAction("List", "Book");
+            return RedirectToAction(nameof(List));
         }
 
-        //Show list function
-        [HttpGet]
-        public async Task<IActionResult> List()
-        {
-            var bookList = await dbContext.Books
-                .Include(b => b.Author)
-                .Include(b => b.Category)
-                .ToListAsync();
-            return View(bookList);
-        }
+        ViewBag.Authors = new SelectList(dbContext.Authors, "AuthorID", "LastName", book.AuthorID);
+        ViewBag.Categories = new SelectList(dbContext.Categories, "CategoryID", "CategoryName", book.CategoryID);
+        return View(book);
+    }
+
+    // GET: Book/List
+    [HttpGet]
+    public async Task<IActionResult> List()
+    {
+        var bookList = await dbContext.Books
+            .Include(b => b.Author)
+            .Include(b => b.Category)
+            .ToListAsync();
+        return View(bookList);
     }
 }
