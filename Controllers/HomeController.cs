b358fdc404d2b691c98f8a48dc3c6615115381bb
HoQuangDat <hoquangdat123@gmail.com>
2025-08-28 16:31:05 +0700

    Add memory caching to HomeController Index action for book list performance improvement

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,22 +1,32 @@
 using System.Diagnostics;
 using BookStore.Data;
+using BookStore.DataModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace BookStore.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ApplicationDbContext _db;
-    public HomeController(ApplicationDbContext db)
+    private readonly IMemoryCache _cache;
+    public HomeController(ApplicationDbContext db, IMemoryCache cache)
     {
         _db = db;
+        _cache = cache;
     }
-
 
     public IActionResult Index()
     {
-        var indexBook = _db.Books.ToList();
+        const string cacheKey = "BookList";
+        if (!_cache.TryGetValue(cacheKey, out List<Book> indexBook))
+        {
+            indexBook = _db.Books.ToList();
+            var cacheEntryOptions = new MemoryCacheEntryOptions()
+                .SetSlidingExpiration(TimeSpan.FromMinutes(5));
+            _cache.Set(cacheKey, indexBook, cacheEntryOptions);
+        }
         return View(indexBook);
     }
 }
